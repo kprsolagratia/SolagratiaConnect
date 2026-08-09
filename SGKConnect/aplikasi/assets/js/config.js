0106cdcf22f20148d3ea92eba5f4e2d4288658ed
kprsolagratia <kprsolagratia@gmail.com>
2026-08-09 23:14:49 +0800

    Fix comment formatting in config.js

--- SGKConnect/aplikasi/assets/js/config.js
+++ SGKConnect/aplikasi/assets/js/config.js
@@ -9,7 +9,7 @@
      • Cloudflare → Settings › Variables and Secrets
 
    Dibuat: 2026-08-01T14:59:10.666Z
-   ========================================================= */
+   ======================================================= */
 window.SGK_CONFIG = {
   "SUPABASE_URL": "https://pamtltjnzuynkchjokun.supabase.co",
   "SUPABASE_ANON_KEY": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBhbXRsdGpuenV5bmtjaGpva3VuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU1ODc0MjIsImV4cCI6MjEwMTE2MzQyMn0.eQiMjxxBQEHLQdZi54G6oAMtxxswh5kFBOl4qpvIamU",
